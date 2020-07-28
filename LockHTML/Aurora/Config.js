131d5f8f85f40ce7ff9d37248176e64e58efa688
Brian <31559308+BrianLeek@users.noreply.github.com>
2020-07-27 18:04:12 -0600

    Updated to 1.1
    Aurora has been updated to 1.1:
    
     - Added weather and battery percentage.
     - Added customization settings.

---
+++ LockHTML/Aurora/Config.js
@@ -0,0 +1,18 @@
+/*
+Configurations
+Credits: Original code by Dacal & Junesiphone. Modified by Evelyn. Modified again by Novinha, and again by Brian.
+*/
+
+var Weather = true; //Display weather "true" or "false".
+var WeatherColor = "ffffff"; // Color is in hexadecimal format.
+
+var Clock = "12h"; //Display time in "12h" or "24h".
+var ClockColor = "ffffff"; // Color is in hexadecimal format.
+
+var Cal = true; //Display day "true" or "false".
+var CalColor = "ffffff"; // Color is in hexadecimal format.
+
+var LevelDisplay = true; //Display battery % "true" or "false".
+var LevelDisplayColor = "ffffff"; // Color is in hexadecimal format.
+
+var Lang = "en"; // Choose language "en", "es", "ca", "fr", "de", "it", "ru", "pl", "pt", "cz", "no", "nl", "fi", "cn", "zh".
--- LockHTML/Aurora/scripts/custom.js
+++ LockHTML/Aurora/scripts/custom.js
@@ -7,29 +7,4 @@ $(document).ready(() => {
 
   $('#date').html(`${dayNames[newDate.getDay()]}<br><div id="day-month">${newDate.getDate()} ${monthNames[newDate.getMonth()]}</div>`);
 
-  window.onload = function () {
-    clock();
-    function clock() {
-      const now = new Date();
-      const TwentyFourHour = now.getHours();
-      let hour = now.getHours();
-      let min = now.getMinutes();
-      const seconds = new Date().getSeconds();
-      let mid = 'pm';
-      if (min < 10) {
-        min = `0${min}`;
-      }
-      if (hour > 12) { // If you want the time to be in 24 hour format change "12" to "24".
-        hour -= 12;
-      }
-      if (hour == 0) {
-        hour = 12;
-      }
-      if (TwentyFourHour < 12) {
-        mid = 'am';
-      }
-      document.getElementById('time').innerHTML = `${hour}:${min}`;
-      setTimeout(clock, 1000);
-    }
-  };
 });
